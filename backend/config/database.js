7a311a1aeaf8025ef4fd0496547682815eaaa5f9
DESKTOP-1IUFQNB\Asus <nightpain@live.com>
2024-05-18 22:03:54 +0200

    database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 function connectToDatabase () {
-    mongoose.connect('mongodb://0.0.0.0:27017/meanstackapp', { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb://127.0.0.1:27017/meanstackapp', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err))
 }
